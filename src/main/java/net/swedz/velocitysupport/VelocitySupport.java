f181468449fa2cc6eea9e92ddc6760934dac6906
Swedz <me@swedz.net>
2025-12-29 20:34:05 -0500

    Better error case handling on startup

--- src/main/java/net/swedz/velocitysupport/VelocitySupport.java
+++ src/main/java/net/swedz/velocitysupport/VelocitySupport.java
@@ -1,11 +1,14 @@
 package net.swedz.velocitysupport;
 
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.dedicated.DedicatedServer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +34,28 @@ public final class VelocitySupport
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 	
 	public VelocitySupport(IEventBus bus, ModContainer container)
+	{
+		NeoForge.EVENT_BUS.addListener(ServerAboutToStartEvent.class, (event) ->
+		{
+			if(event.getServer() instanceof DedicatedServer server)
+			{
+				if(server.getProperties().onlineMode)
+				{
+					LOGGER.warn("Server loaded in online mode! Your proxy will be unable to connect to this server like this. Set online-mode=false in your server.properties file.");
+				}
+				else
+				{
+					setup();
+				}
+			}
+			else
+			{
+				LOGGER.warn("Loaded on a non-dedicated server... why!? This will do nothing for you.");
+			}
+		});
+	}
+	
+	private static void setup()
 	{
 		try
 		{
